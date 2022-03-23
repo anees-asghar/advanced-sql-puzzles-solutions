eb2e95eecff420d238b5adcbe127b45df80868a4
Anees Asghar <aneesasgharsyed@gmail.com>
2022-03-23 22:30:45 +0000

    First Commit (24 problems solved)

---
+++ ddl.sql
@@ -0,0 +1,552 @@
+-- DDL for Advanced SQL Puzzles by Scott Peters
+
+CREATE SCHEMA IF NOT EXISTS advanced_sql_puzzles;
+USE advanced_sql_puzzles;
+
+
+/*
+DDL for Puzzle #1
+Shopping Carts
+*/
+DROP TABLE IF EXISTS Cart1;
+DROP TABLE IF EXISTS Cart2;
+CREATE TABLE Cart1
+(
+	Item VARCHAR(100) PRIMARY KEY
+);
+CREATE TABLE Cart2
+(
+	Item VARCHAR(100) PRIMARY KEY
+);
+INSERT INTO Cart1 (Item) VALUES
+('Sugar'),('Bread'),('Juice'),('Soda'),('Flour');
+INSERT INTO Cart2 VALUES
+('Sugar'),('Bread'),('Butter'),('Cheese'),('Fruit');
+
+
+/*
+DDL for Puzzle #2
+Managers and Employees
+*/
+DROP TABLE IF EXISTS Employees;
+CREATE TABLE Employees
+(
+EmployeeID  INTEGER PRIMARY KEY,
+ManagerID   INTEGER,
+JobTitle    VARCHAR(100),
+Salary      INTEGER
+);
+
+INSERT INTO Employees VALUES
+(1001,NULL,'President',185000),(2002,1001,'Director',120000),
+(3003,1001,'Office Manager',97000),(4004,2002,'Engineer',110000),
+(5005,2002,'Engineer',142000),(6006,2002,'Engineer',160000);
+
+
+/*
+Answer to Puzzle #3
+Fiscal Year Table Constraints
+*/
+DROP TABLE IF EXISTS EmployeePayRecords;
+CREATE TABLE EmployeePayRecords
+(
+EmployeeID  INTEGER,
+FiscalYear  INTEGER,
+StartDate   DATE,
+EndDate     DATE,
+PayRate     INT
+);
+
+
+/*
+DDL for Puzzle #4
+Two Predicates
+*/
+DROP TABLE IF EXISTS Orders;
+CREATE TABLE Orders
+(
+CustomerID      INTEGER,
+OrderID         VARCHAR(100),
+DeliveryState   VARCHAR(100),
+Amount          INTEGER,
+PRIMARY KEY (CustomerID, OrderID)
+);
+INSERT INTO Orders VALUES
+(1001,'Ord936254','CA',340),(1001,'Ord143876','TX',950),(1001,'Ord654876','TX',670),
+(1001,'Ord814356','TX',860),(2002,'Ord342176','WA',320),(3003,'Ord265789','CA',650),
+(3003,'Ord387654','CA',830),(4004,'Ord476126','TX',120);
+
+
+/*
+DDL for Puzzle #5
+Phone Directory
+*/
+DROP TABLE IF EXISTS PhoneDirectory;
+CREATE TABLE PhoneDirectory
+(
+CustomerID  INTEGER,
+type        VARCHAR(100),
+PhoneNumber VARCHAR(12),
+PRIMARY KEY (CustomerID, type)
+);
+INSERT INTO PhoneDirectory VALUES
+(1001,'Cellular','555-897-5421'),
+(1001,'Work','555-897-6542'),
+(1001,'Home','555-698-9874'),
+(2002,'Cellular','555-963-6544'),
+(2002,'Work','555-812-9856'),
+(3003,'Cellular','555-987-6541');
+
+
+/*
+DDL for Puzzle #6
+Workflow Steps
+*/
+DROP TABLE IF EXISTS WorkflowSteps;
+CREATE TABLE WorkflowSteps
+(
+Workflow        VARCHAR(100),
+StepNumber      INTEGER,
+CompletionDate  DATE,
+PRIMARY KEY (Workflow, StepNumber)
+);
+INSERT INTO WorkflowSteps VALUES
+('Alpha',1,'2018-07-02'),('Alpha',2,'2018-07-02'),('Alpha',3,'2018-07-01'),
+('Bravo',1,'2018-06-25'),('Bravo',2,NULL),('Bravo',3,'2018-06-27'),
+('Charlie',1,NULL),('Charlie',2,'2018-07-01');
+
+
+/*
+DDL for Puzzle #7
+Mission to Mars
+*/
+DROP TABLE IF EXISTS Candidates;
+DROP TABLE IF EXISTS Requirements;
+CREATE TABLE Candidates
+(
+CandidateID INTEGER,
+Occupation  VARCHAR(100),
+PRIMARY KEY (CandidateID, Occupation)
+);
+INSERT INTO Candidates VALUES
+(1001,'Geologist'),(1001,'Astrogator'),(1001,'Biochemist'),
+(1001,'Technician'),(2002,'Surgeon'),(2002,'Machinist'),
+(3003,'Cryologist'),(4004,'Selenologist');
+CREATE TABLE Requirements
+(
+Requirement VARCHAR(100) PRIMARY KEY
+);
+INSERT INTO Requirements VALUES
+('Geologist'),('Astrogator'),('Technician');
+
+
+/*
+DDL for Puzzle #8
+Workflow Cases
+*/
+DROP TABLE IF EXISTS WorkflowCases;
+CREATE TABLE WorkflowCases
+(
+Workflow    VARCHAR(100) PRIMARY KEY,
+Case1       INTEGER,
+Case2       INTEGER,
+Case3       INTEGER
+);
+INSERT INTO WorkflowCases VALUES
+('Alpha',0,0,0),('Bravo',0,1,1),('Charlie',1,0,0),('Delta',0,0,0);
+
+
+/*
+DDL for Puzzle #9
+Matching Sets
+*/
+DROP TABLE IF EXISTS Employees;
+CREATE TABLE Employees
+(
+EmployeeID  INTEGER,
+License     VARCHAR(100),
+PRIMARY KEY (EmployeeID, License)
+);
+INSERT INTO Employees VALUES
+(1001,'Class A'),
+(1001,'Class B'),
+(1001,'Class C'),
+(2002,'Class A'),
+(2002,'Class B'),
+(2002,'Class C'),
+(3003,'Class A'),
+(3003,'Class D');
+
+
+/*
+DDL for Puzzle #10
+Mean, Median, Mode and Range
+*/
+DROP TABLE IF EXISTS SampleData;
+CREATE TABLE SampleData
+(
+IntegerValue INTEGER
+);
+INSERT INTO SampleData VALUES
+(5),(6),(10),(10),(13),(14),(17),(20),(81),(90),(76);
+
+
+/*
+DDL for Puzzle #11
+*/
+DROP TABLE IF EXISTS TestCases;
+
+CREATE TABLE TestCases
+(
+RowNumber INTEGER,
+TestCase VARCHAR(1),
+PRIMARY KEY (RowNumber, TestCase)
+);
+
+INSERT INTO TestCases VALUES
+(1,'A'),(2,'B'),(3,'C');
+
+
+/*
+DDL for Puzzle #12
+Average Days
+*/
+DROP TABLE IF EXISTS ProcessLog;
+
+CREATE TABLE ProcessLog
+(
+WorkFlow        VARCHAR(100),
+ExecutionDate   DATE,
+PRIMARY KEY (WorkFlow, ExecutionDate)
+);
+
+INSERT INTO ProcessLog VALUES
+('Alpha','2018-06-01'),('Alpha','2018-06-14'),('Alpha','2018-06-15'),
+('Bravo','2018-06-01'),('Bravo','2018-06-02'),('Bravo','2018-06-19'),
+('Charlie','2018-06-01'),('Charlie','2018-06-15'),('Charlie','2018-06-30');
+
+
+/*
+DDL for Puzzle #13
+Inventory Tracking
+*/
+DROP TABLE IF EXISTS Inventory;
+
+CREATE TABLE Inventory
+(
+InventoryDate       DATE PRIMARY KEY,
+QuantityAdjustment  INTEGER
+);
+
+INSERT INTO Inventory VALUES
+('2018-7-1',100),('2018-7-2',75),('2018-7-3',-150),
+('2018-7-4',50),('2018-7-5',-75);
+
+
+/*
+DDL for Puzzle #14
+Indeterminate Process Log
+*/
+DROP TABLE IF EXISTS ProcessLog;
+
+CREATE TABLE ProcessLog
+(
+Workflow    VARCHAR(100),
+StepNumber  INTEGER,
+Status    VARCHAR(100),
+PRIMARY KEY (Workflow, StepNumber)
+);
+
+INSERT INTO ProcessLog VALUES
+('Alpha',1,'Error'),('Alpha',2,'Complete'),('Bravo',1,'Complete'),('Bravo',2,'Complete'),
+('Charlie',1,'Complete'),('Charlie',2,'Error'),('Delta',1,'Complete'),('Delta',2,'Running'),
+('Echo',1,'Running'),('Echo',2,'Error'),('Foxtrot',1,'Error'),('Foxtrot',2,'Error');
+
+
+/*
+DDL for Puzzle #15
+Group Concatenation
+*/
+DROP TABLE IF EXISTS DMLTable;
+
+CREATE TABLE DMLTable
+(
+SequenceNumber  INTEGER PRIMARY KEY,
+String          VARCHAR(100)
+);
+
+INSERT INTO DMLTable VALUES
+(1,'SELECT'),
+(5,'FROM'),
+(7,'WHERE'),
+(2,'Product'),
+(6,'Products'),
+(3,'UnitPrice'),
+(9,'> 100'),
+(4,'EffectiveDate'),
+(8,'UnitPrice');
+
+/*
+DDL for Puzzle #16
+Reciprocals
+*/
+DROP TABLE IF EXISTS PlayerScores;
+
+CREATE TABLE PlayerScores
+(
+PlayerA INTEGER,
+PlayerB INTEGER,
+Score   INTEGER,
+PRIMARY KEY (PlayerA, PlayerB)
+);
+
+INSERT INTO PlayerScores VALUES
+(1001,2002,150),(3003,4004,15),
+(4004,3003,125),(4004,1001,125);
+
+
+/*
+DDL for Puzzle #17
+De-Grouping
+*/
+DROP TABLE IF EXISTS Ungroup;
+
+CREATE TABLE Ungroup
+(
+ProductDescription  VARCHAR(100) PRIMARY KEY,
+Quantity            INTEGER
+);
+
+INSERT INTO Ungroup VALUES
+('Eraser',3),('Pencil',4),('Sharpener',2);
+
+/*
+DDL for Puzzle #18
+Seating Chart
+*/
+DROP TABLE IF EXISTS SeatingChart;
+
+CREATE TABLE SeatingChart
+(
+SeatNumber INTEGER PRIMARY KEY
+);
+
+INSERT INTO SeatingChart VALUES
+(7),(13),(14),(15),(27),(28),(29),(30),(31),(32),(33),(34),(35),(52),(53),(54);
+
+
+/*
+DDL for Puzzle #19
+Back to the Future
+*/
+DROP TABLE IF EXISTS TimePeriods;
+
+CREATE TABLE TimePeriods
+(
+StartDate   DATE,
+EndDate     DATE
+);
+
+INSERT INTO TimePeriods VALUES
+('2018-1-1','2018-1-5'),('2018-1-1','2018-1-3'),
+('2018-1-1','2018-1-2'),('2018-1-3','2018-1-9'),
+('2018-1-10','2018-1-11'),('2018-1-12','2018-1-16'),
+('2018-1-15','2018-1-19');
+
+
+/*
+DDL for Puzzle #20
+Price Points
+*/
+DROP TABLE IF EXISTS ValidPrices;
+
+CREATE TABLE ValidPrices
+(
+ProductID       INTEGER,
+UnitPrice       DECIMAL(4, 2),
+EffectiveDate   DATE,
+PRIMARY KEY (ProductID, UnitPrice, EffectiveDate)
+);
+
+INSERT INTO ValidPrices VALUES
+(1001,1.99,'2018-01-01'),
+(1001,2.99,'2018-04-15'),
+(1001,3.99,'2018-06-08'),
+(2002,1.99,'2018-04-17'),
+(2002,2.99,'2018-05-19');
+
+
+/*
+DDL for Puzzle #21
+Average Monthly Sales
+*/
+DROP TABLE IF EXISTS Orders;
+
+CREATE TABLE Orders
+(
+OrderID     VARCHAR(100) PRIMARY KEY,
+CustomerID  INTEGER,
+OrderDate   DATE,
+Amount      INT,
+State     VARCHAR(2)
+);
+
+INSERT INTO Orders VALUES
+('Ord145332',1001,'2018-01-01',100,'TX'),
+('Ord657895',1001,'2018-01-01',150,'TX'),
+('Ord887612',1001,'2018-01-01',75,'TX'),
+('Ord654374',1001,'2018-02-01',100,'TX'),
+('Ord345362',1001,'2018-03-01',100,'TX'),
+('Ord912376',2002,'2018-02-01',75,'TX'),
+('Ord543219',2002,'2018-02-01',150,'TX'),
+('Ord156357',3003,'2018-01-01',100,'IA'),
+('Ord956541',3003,'2018-02-01',100,'IA'),
+('Ord856993',3003,'2018-03-01',100,'IA'),
+('Ord864573',4004,'2018-04-01',100,'IA'),
+('Ord654525',4004,'2018-05-01',50,'IA'),
+('Ord987654',4004,'2018-05-01',100,'IA');
+
+
+/*
+DDL for Puzzle #22
+Occurrences
+*/
+DROP TABLE IF EXISTS ProcessLog;
+
+CREATE TABLE ProcessLog
+(
+Workflow    VARCHAR(100),
+LogMessage  VARCHAR(100),
+Occurrences INTEGER,
+PRIMARY KEY (Workflow, LogMessage)
+);
+
+INSERT INTO ProcessLog VALUES
+('Alpha','Error: Conversion Failed',5),
+('Alpha','Status Complete',8),
+('Alpha','Error: Unidentified error occurred',9),
+('Bravo','Error: Cannot Divide by 0',3),
+('Bravo','Error: Unidentified error occurred',1),
+('Charlie','Error: Unidentified error occurred',10),
+('Charlie','Error: Conversion Failed',7),
+('Charlie','Status Complete',6);
+
+
+/*
+DDL for Puzzle #23
+Divide in Half
+*/
+DROP TABLE IF EXISTS PlayerScores;
+
+CREATE TABLE PlayerScores
+(
+PlayerID    INTEGER PRIMARY KEY,
+Score       INTEGER
+);
+
+INSERT INTO PlayerScores VALUES
+(1001,2343),(2002,9432),
+(3003,6548),(4004,1054),
+(5005,6832);
+
+
+/*
+DDL for Puzzle #24
+Page Views
+*/
+DROP TABLE IF EXISTS SampleData;
+
+CREATE TABLE SampleData
+(
+RowID           VARCHAR(36)
+);
+
+DROP PROCEDURE IF EXISTS insert_n_rows_in_sample_data;
+DELIMITER //
+CREATE PROCEDURE insert_n_rows_in_sample_data (IN n INT)
+BEGIN
+	DECLARE x INT DEFAULT 1;
+	WHILE x <= n DO
+		SET x = x+1;
+        INSERT INTO SampleData VALUES(UUID());
+    END WHILE;
+END//
+DELIMITER ;
+
+CALL insert_n_rows_in_sample_data(1000);
+
+
+/*
+DDL for Puzzle #25
+Top Vendors
+*/
+DROP TABLE IF EXISTS Orders;
+
+CREATE TABLE Orders
+(
+OrderID     VARCHAR(100) PRIMARY KEY,
+CustomerID  INTEGER,
+OrderCount  INT,
+Vendor      VARCHAR(100)
+);
+
+INSERT INTO Orders VALUES
+('Ord195342',1001,12,'Direct Parts'),
+('Ord245532',1001,54,'Direct Parts'),
+('Ord344394',1001,32,'ACME'),
+('Ord442423',2002,7,'ACME'),
+('Ord524232',2002,16,'ACME'),
+('Ord645363',2002,5,'Direct Parts');
+
+
+/*
+DDL for Puzzle #26
+Previous Years Sales
+*/
+DROP TABLE IF EXISTS Sales;
+
+CREATE TABLE Sales
+(
+Year  INTEGER,
+Amount  INTEGER
+);
+
+INSERT INTO Sales VALUES
+(YEAR(NOW()),352645),
+(YEAR(DATE_SUB(NOW(), INTERVAL 1 YEAR)),165565),
+(YEAR(DATE_SUB(NOW(), INTERVAL 1 YEAR)),254654),
+(YEAR(DATE_SUB(NOW(), INTERVAL 2 YEAR)),159521),
+(YEAR(DATE_SUB(NOW(), INTERVAL 2 YEAR)),251696),
+(YEAR(DATE_SUB(NOW(), INTERVAL 3 YEAR)),111894);
+
+
+/*
+DDL for Puzzle #27
+Delete the Duplicates
+*/
+DROP TABLE IF EXISTS SampleData;
+
+CREATE TABLE SampleData
+(
+IntegerValue INTEGER
+);
+
+INSERT INTO SampleData VALUES
+(1),(1),(2),(3),(3),(4);
+
+
+/*
+DDL for Puzzle #28
+Fill the Gaps
+*/
+DROP TABLE IF EXISTS Gaps;
+
+CREATE TABLE Gaps
+(
+RowNumber   INTEGER PRIMARY KEY,
+TestCase    VARCHAR(100)
+);
+
+INSERT INTO Gaps VALUES
+(1,'Alpha'),(2,NULL),(3,NULL),(4,NULL),
+(5,'Bravo'),(6,NULL),(7,'Charlie'),(8,NULL),(9,NULL);
---
+++ solutions.sql
@@ -0,0 +1,383 @@
+-- Solutions to Advanced SQL Puzzles by Scott Peters
+
+/*
+Solution Puzzle #1
+Shopping Carts
+*/
+SELECT c1.Item, c2.Item
+FROM Cart1 c1
+LEFT JOIN Cart2 c2 USING (Item)
+UNION
+SELECT c1.Item, c2.Item
+FROM Cart1 c1
+RIGHT JOIN Cart2 c2 USING (Item);
+
+
+/*
+Solution to Puzzle #2
+Managers and Employees
+*/
+WITH RECURSIVE emp_depth AS (
+	SELECT EmployeeID, ManagerID, JobTitle, Salary, 0 depth
+    FROM employees 
+    WHERE ManagerID IS NULL
+    UNION ALL
+    SELECT e.EmployeeID, e.ManagerID, e.JobTitle, e.Salary, ed.depth+1 depth
+    FROM employees e
+    JOIN emp_depth ed ON e.ManagerID = ed.EmployeeID
+)
+SELECT * FROM emp_depth;
+
+
+/*
+Answer to Puzzle #3
+Fiscal Year Table Constraints
+*/
+ALTER TABLE EmployeePayRecords
+	MODIFY EmployeeID INT NOT NULL,
+    MODIFY FiscalYear INT NOT NULL,
+    MODIFY StartDate DATE NOT NULL,
+    MODIFY EndDate DATE NOT NULL,
+    MODIFY PayRate INT NOT NULL;
+
+ALTER TABLE EmployeePayRecords
+ADD PRIMARY KEY (EmployeeID, FiscalYear);
+
+ALTER TABLE EmployeePayRecords
+ADD CHECK (YEAR(StartDate)=FiscalYear AND MONTH(StartDate)=1 AND DATE(StartDate)=01);
+
+ALTER TABLE EmployeePayRecords
+ADD CHECK (YEAR(EndDate)=FiscalYear AND MONTH(EndDate)=12 AND DATE(EndDate)=31);
+
+ALTER TABLE EmployeePayRecords
+ADD CHECK (PayRate >= 0);
+		
+
+/*
+Solution to Puzzle #4
+Two Predicates
+*/
+SELECT * 
+FROM orders o
+WHERE DeliveryState = 'TX' AND
+	EXISTS (
+		SELECT 1 FROM Orders 
+        WHERE CustomerID = o.CustomerID AND 
+			DeliveryState = 'CA'
+	);
+
+
+/*
+Solution to Puzzle #5
+Phone Directory
+*/
+SELECT CustomerID, MAX(Cellular) Cellular, MAX(Work) Work, MAX(Home) Home 
+FROM (
+	SELECT 
+		CustomerID,
+		IF(type='Cellular', PhoneNumber, NULL) AS Cellular,
+		IF(type='Home', PhoneNumber, NULL) AS Home,
+		IF(type='Work', PhoneNumber, NULL) AS Work
+	FROM phonedirectory
+) AS tmp
+GROUP BY CustomerID;
+
+
+/*
+Solution to Puzzle #6
+Workflow Steps
+*/
+SELECT Workflow
+FROM workflowsteps
+GROUP BY Workflow
+HAVING COUNT(CompletionDate) !=  MAX(StepNumber);
+
+
+/*
+Solution to Puzzle #7
+Mission to Mars
+*/
+SELECT CandidateID
+FROM Candidates c 
+JOIN Requirements r ON c.Occupation = r.Requirement
+GROUP BY CandidateID
+HAVING COUNT(*) = 3;
+
+
+/*
+Solution to Puzzle #8
+Workflow Cases
+*/
+SELECT Workflow, (Case1 + Case2 + Case3) AS Passed
+FROM workflowcases;
+
+
+/*
+Solution to Puzzle #9
+Matching Sets
+*/
+SELECT e1.EmployeeID, e2.EmployeeID EmployeeID2, COUNT(*) LicenseCountCombo 
+FROM Employees e1
+JOIN Employees e2 USING (License)
+WHERE e1.EmployeeID != e2.EmployeeID
+GROUP BY e1.EmployeeID, e2.EmployeeID
+HAVING LicenseCountCombo = (
+	SELECT COUNT(1) 
+    FROM Employees 
+    WHERE EmployeeID = e1.EmployeeID
+);
+
+
+/*
+Solution to Puzzle #10
+Mean, Median, Mode and Range
+*/
+SELECT AVG(IntegerValue) 
+INTO @average
+FROM SampleData;
+
+SET @size = (SELECT COUNT(*) FROM SampleData);
+SELECT AVG(IntegerValue)
+INTO @median
+FROM (
+	SELECT IntegerValue, ROW_NUMBER() OVER (ORDER BY IntegerValue) AS row_num
+	FROM SampleData
+) AS t
+WHERE CASE WHEN @size MOD 2 
+	THEN row_num =  CEIL(@size/2)
+    ELSE row_num = @size/2 OR row_num = @size/2+1
+    END;
+
+SELECT IntegerValue
+INTO @mode
+FROM SampleData
+GROUP BY IntegerValue
+ORDER BY COUNT(*) DESC
+LIMIT 1;
+
+SELECT MAX(IntegerValue) - MIN(IntegerValue) 
+INTO @range
+FROM SampleData;
+
+SELECT ROUND(@average,2) average, ROUND(@median,2) median, @mode 'mode', @range 'range';
+
+
+/*
+Answer to Puzzle #11
+Permutations 
+*/
+# To be completed
+
+
+/*
+Answer to Puzzle #12
+Average Days
+*/
+SELECT WorkFlow, FLOOR(AVG(DaysToNextEntry)) AvgDays
+FROM (
+	SELECT *, DATEDIFF(LEAD(ExecutionDate) OVER (PARTITION BY WorkFlow), ExecutionDate) DaysToNextEntry
+	FROM ProcessLog
+) AS t
+GROUP BY WorkFlow;
+
+
+/*
+Answer to Puzzle #13
+Inventory Tracking
+*/
+SELECT *, SUM(QuantityAdjustment) OVER (ORDER BY InventoryDate) 
+FROM inventory;
+
+
+/*
+Answer to Puzzle #14
+Indeterminate Process Log
+*/
+SELECT Workflow, (
+	CASE WHEN 'Complete' = ALL (SELECT Status FROM ProcessLog WHERE Workflow = pl.Workflow)
+		THEN 'Complete' 
+    WHEN 'Error' = ALL (SELECT Status FROM ProcessLog WHERE Workflow = pl.Workflow)
+		THEN 'Error'
+	WHEN 'Running' = ALL (SELECT Status FROM ProcessLog WHERE Workflow = pl.Workflow) OR 
+		'Error' NOT IN (SELECT Status FROM ProcessLog WHERE Workflow = pl.Workflow)
+		THEN 'Running'
+    WHEN 'Running' NOT IN (SELECT Status FROM ProcessLog WHERE Workflow = pl.Workflow) OR
+		'Complete' NOT IN (SELECT Status FROM ProcessLog WHERE Workflow = pl.Workflow)
+		THEN 'Indeterminate'
+	END
+) as Status
+FROM ProcessLog pl
+GROUP BY Workflow;
+
+
+/*
+Answer to Puzzle #15
+Group Concatenation
+*/
+SELECT GROUP_CONCAT(String SEPARATOR ' ')
+FROM (
+	SELECT	
+		(
+			CASE WHEN SequenceNumber IN (2, 3)
+			THEN CONCAT(String, ',')
+			ELSE String
+			END
+		) AS String
+	FROM dmltable
+) AS t;
+
+
+/*
+Answer to Puzzle #16
+Reciprocals
+*/
+SELECT PlayerA, PlayerB, SUM(Score) AS Score
+FROM (
+	SELECT 
+		(CASE WHEN PlayerA < PlayerB THEN PlayerA ELSE PlayerB END) AS PlayerA,
+		(CASE WHEN PlayerA < PlayerB THEN PlayerB ELSE PlayerA END) AS PlayerB,
+		Score
+	FROM playerscores
+) AS t
+GROUP BY PlayerA, PlayerB;
+
+
+/*
+Answer to Puzzle #17
+De-grouping
+*/
+# To be completed
+
+
+/*
+Answer to Puzzle #18
+Seating Chart
+*/
+SET @start = 1;
+SELECT gapStart 'Gap Start', gapEnd 'Gap End'
+FROM (
+	SELECT @start 'gapStart', SeatNumber-1 'gapEnd', @start:=SeatNumber+1 
+	FROM SeatingChart
+) AS t
+WHERE gapStart < gapEnd;
+
+
+/*
+Answer to Puzzle #19
+Back to the Future
+*/
+# To be completed
+
+
+/*
+Answer to Puzzle #20
+Price Points
+*/
+SELECT t.ProductID, t.EffectiveDate, UnitPrice
+FROM (
+	SELECT ProductID, MAX(EffectiveDate) EffectiveDate
+	FROM ValidPrices
+	GROUP BY ProductID
+) AS t
+JOIN ValidPrices v
+	ON v.ProductID = t.ProductID AND v.EffectiveDate = t.EffectiveDate;
+    
+
+/*
+Answer to Puzzle #21
+Average Monthly Sales
+*/
+WITH MonthlySales AS (
+	SELECT State, CustomerID, EXTRACT(MONTH FROM OrderDate) OrderMonth, SUM(Amount) MonthlySales
+	FROM Orders
+	GROUP BY State, CustomerID, orderMonth
+	ORDER BY CustomerID, OrderMonth
+), AvgMonthlySales AS (
+	SELECT State, CustomerID, AVG(MonthlySales) AvgMonthlySales
+	FROM MonthlySales
+	GROUP BY State, CustomerID
+)
+SELECT State
+FROM AvgMonthlySales t
+GROUP BY State
+HAVING 100 < ALL(SELECT AvgMonthlySales FROM AvgMonthlySales WHERE State = t.State);
+
+
+/*
+Answer to Puzzle #22
+Occurences
+*/
+SELECT Workflow, LogMessage 
+FROM (
+	SELECT LogMessage, Workflow, Occurrences, ROW_NUMBER() OVER (PARTITION BY LogMessage ORDER BY Occurrences DESC) row_num
+	FROM ProcessLog
+) AS t
+WHERE row_num = 1;
+
+
+/*
+Answer to Puzzle #23
+Divide in Half
+*/
+SELECT IF(
+	COUNT(1) MOD 2, -- ODD
+    FLOOR(COUNT(1)/2), 
+    COUNT(1) / 2
+    ) 
+INTO @half
+FROM PlayerScores;
+
+SELECT PlayerID, Score, 
+	CASE WHEN row_num <= @half THEN 1 ELSE 2 END 'Group'
+FROM (
+	SELECT *, ROW_NUMBER() OVER (ORDER BY Score DESC) row_num 
+	FROM PlayerScores
+) AS t;
+
+
+/*
+Answer to Puzzle #24
+Page Views
+*/
+SELECT *
+FROM SampleData
+ORDER BY RowID
+LIMIT 10, 10;
+
+
+/*
+Answer to Puzzle #25
+Top Vendors
+*/
+SELECT CustomerID, Vendor
+FROM Orders o
+WHERE OrderCount = (SELECT MAX(OrderCount) FROM Orders WHERE CustomerID = o.CustomerID);
+
+
+/*
+Answer to Puzzle #26
+Previous Year's Sales
+*/
+SELECT SUM(Year_2019) '2019', SUM(Year_2020) '2020', SUM(Year_2021) '2021', SUM(Year_2022) '2022'
+FROM (
+	SELECT 
+		CASE WHEN Year = 2019 THEN Amount ELSE NULL END Year_2019,
+		CASE WHEN Year = 2020 THEN Amount ELSE NULL END Year_2020,
+		CASE WHEN Year = 2021 THEN Amount ELSE NULL END Year_2021,
+		CASE WHEN Year = 2022 THEN Amount ELSE NULL END Year_2022
+	FROM Sales
+) AS t;
+
+/*
+Answer to Puzzle #27
+Delete the duplicates
+*/
+# To be completed
+
+
+/*
+Answer to Puzzle #28
+Fill the Gaps
+*/
+SELECT RowNumber, @currTestCase := (CASE WHEN TestCase IS NOT NULL THEN TestCase ELSE @currTestCase END) Workflow
+FROM Gaps;
